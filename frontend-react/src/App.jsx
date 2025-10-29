2c8b596600f72b52aac8e728cecd7274751392f6
chirayuchavhan <chirayuchavhan@gmail.com>
2025-10-29 17:59:20 +0530

    authentication functionalities

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,22 +6,28 @@ import Register from './components/Register'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Login from './components/Login'
+import AuthProvider from './AuthProvider'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 
 function App() {
 
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
       <Header />
         <Routes>
           <Route path='/' element={< Main />} />
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<PublicRoute> <Register/> </PublicRoute>}/>
+          <Route path='/login' element={<PublicRoute> <Login/> </PublicRoute>}/>
+          <Route path='/dashboard' element={<PrivateRoute> <Dashboard /> </PrivateRoute>}/>
         </Routes>
       <Footer />
       </BrowserRouter>
-      
+    </AuthProvider>
       
     </>
   )
